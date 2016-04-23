63661519f232f67ec5c82a5201e3da15949418c4
dollar007 <vlad.karapetyan.1988@mail.ru>
2016-04-23 16:32:24 +0400

    Changing uploaded file name function

--- Roundcube_gluu_sso_2.4.2/gluu_sso.php
+++ Roundcube_gluu_sso_2.4.2/gluu_sso.php
@@ -1257,4 +1257,27 @@ class gluu_sso extends rcube_plugin
         }
         $RCMAIL->output->redirect('plugin.gluu_sso');
     }
+
+    /*
+     * Changing uploaded file name
+    */
+    function file_newname($path, $filename){
+        if ($pos = strrpos($filename, '.')) {
+            $name = substr($filename, 0, $pos);
+            $ext = substr($filename, $pos);
+        } else {
+            $name = $filename;
+        }
+
+        $newpath = $path.'/'.$filename;
+        $newname = $filename;
+        $counter = 0;
+        while (file_exists($newpath)) {
+            $newname = $name .'_'. $counter . $ext;
+            $newpath = $path.'/'.$newname;
+            $counter++;
+        }
+
+        return $newname;
+    }
 }
